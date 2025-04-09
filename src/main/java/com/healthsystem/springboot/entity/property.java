90de986d70a11195600a215f3bbe61fdf293281a
jiefox <1748542806@qq.com>
2025-04-09 23:39:10 +0800

    Initial commit

---
+++ src/main/java/com/healthsystem/springboot/entity/property.java
@@ -0,0 +1,42 @@
+//package com.healthsystem.springboot.entity;
+//
+////物业
+//
+//public class property {
+//    private Integer property_id;
+//    private String property_name;
+//    private String password;
+//    private String contact;
+//
+//    public Integer getProperty_id() {
+//        return property_id;
+//    }
+//
+//    public void setProperty_id(Integer property_id) {
+//        this.property_id = property_id;
+//    }
+//
+//    public String getProperty_name() {
+//        return property_name;
+//    }
+//
+//    public void setProperty_name(String property_name) {
+//        this.property_name = property_name;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getContact() {
+//        return contact;
+//    }
+//
+//    public void setContact(String contact) {
+//        this.contact = contact;
+//    }
+//}
