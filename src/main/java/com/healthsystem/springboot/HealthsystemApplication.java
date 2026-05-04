76e18d6c5f0b964ecb6489e63c0778906c628e5a
jiefox <1748542806@qq.com>
2026-05-04 23:39:10 +0800

    Initial commit

---
+++ src/main/java/com/healthsystem/springboot/HealthsystemApplication.java
@@ -0,0 +1,14 @@
+package com.healthsystem.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthsystemApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HealthsystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/healthsystem/springboot/pojo/user.java
@@ -0,0 +1,67 @@
+package com.healthsystem.springboot.pojo;
+
+public class user {
+    private Integer user_id;
+    private String name;
+    private Integer age;
+    private String address;
+    private String contact;
+    private String username;
+    private String password;
+
+    public Integer getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
