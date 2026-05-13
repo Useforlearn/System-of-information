8ece9302deede77161bbe17e7f7010b79c81a34b
kiki-r1 <3413366014@qq.com>
2026-05-13 22:38:10 +0800

    Initial commit

---
+++ vue/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="padding: 30px 0; text-align: center; line-height: 30px">
+    <div>社区养老系统 客服电话 666-78787878</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data() {
+    return {}
+  },
+  created() {
+
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
